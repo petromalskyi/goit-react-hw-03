aeac11ec2fc6204afaafe2c9987c33974f1b9395
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2024-02-25 10:56:35 +0200

    add localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactForm from '../ContactForm/ContactForm';
 import css from './App.module.css';
 
-const phones = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const getStartOption = () => {
+  const phones = window.localStorage.getItem('phone-book');
+  return phones ? JSON.parse(phones) : [];
+};
 
 export default function App() {
-  const [contactsState, setContactsState] = useState(phones);
+  const [contactsState, setContactsState] = useState(getStartOption);
   const [filterState, setFilterState] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('phone-book', JSON.stringify(contactsState));
+  }, [contactsState]);
 
   const addContact = newContact => {
     setContactsState(prevContact => {
